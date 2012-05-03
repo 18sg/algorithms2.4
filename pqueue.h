5317fbd219a48a21681707254206411a0932fc1a
Karl Sutt <karl@sutt.ee>
2012-05-03 20:31:32 +0100

    Comments.

--- pqueue.h
+++ pqueue.h
@@ -4,12 +4,24 @@
 node_t **queue;
 int q_size;
 
-void queue_setup(graph_t *);
+/**
+ * Internal queue functions
+ */
 int h_parent_of(int);
 int h_child_of(int);
 void h_upheap(int);
-void push(node_t *);
 void h_downheap(int);
+
+/**
+ * Functions part of the API.
+ * queue_setup() to allocate space
+ * queue_free() to free the space up
+ * push() to push an element to the queu
+ * pop() to pop the best one off
+ */
+void queue_setup(graph_t *);
+void queue_free(void):
+void push(node_t *);
 node_t *pop();
 
 #endif
--- stats.h
+++ stats.h
@@ -1,13 +1,19 @@
 #ifndef STATS_H
 #define STATS_H
 
+/**
+ * Some relevant values for stats
+ */
 int total_dist;
 double average_distance;
 int non_reachable_nodes;
-double nodes_distance_5;
-double nodes_distance_6;
 int max_distance;
 
+/*
+ * Fills the above values in.
+ * get_stats() can be called multiple times to accumulate stats
+ * stats_fprint() does what it says on the tin
+ */
 void get_stats(graph_t *);
 void stats_fprint(FILE *, graph_t *);
 
